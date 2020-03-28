8a5c313d011d66493cd248dec6c0e1dfeea51221
Jishnu Koottala <jishnu@cloudthing.com>
2020-03-28 13:52:26 +0530

    added carousel dots style

--- src/config/theme.js
+++ src/config/theme.js
@@ -6,7 +6,8 @@ export const lightTheme = {
   headerBanner: "linear-gradient(to top, #fdcbf1 0%, #fdcbf1 1%, #e6dee9 100%)",
   headerBannerText: "#222",
   chartText: "#000",
-  statusCardBoxShadow: "3px 3px 18px -14px rgba(0,0,0,1)"
+  statusCardBoxShadow: "3px 3px 18px -14px rgba(0,0,0,1)",
+  inactiveDots: "#000"
 };
 
 export const darkTheme = {
@@ -17,5 +18,6 @@ export const darkTheme = {
   headerBannerText: "#fff",
   gradient: "linear-gradient(#091236, #1E215D)",
   chartText: "#fff",
-  statusCardBoxShadow: "3px 3px 18px -14px rgba(255,255,255,1)"
+  statusCardBoxShadow: "3px 3px 18px -14px rgba(255,255,255,1)",
+  inactiveDots: "#fff"
 };
